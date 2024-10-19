46891d5f1482bca116543bfc08fd7b7c629d37c1
irfan646 <raiirfan917@gmail.com>
2024-10-19 09:30:40 +0500

    updated

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -38,7 +38,7 @@
     <div class="container-xxl position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
             <a href="#" class="navbar-brand p-0">
-                <h1 class="text-primary m-0"><i class="fa fa-utensils me-3"></i>Order And Menu</h1>
+               <img src="{{ asset('front-assets/img/food-logo.jpeg')}}" alt="Logo" class="food-logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars"></span>
